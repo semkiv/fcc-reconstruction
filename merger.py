fd2938d2747cd76b8c236946900e3cc5a15406a9
Andrii Semkiv <semkiv@gmail.com>
2016-04-01 12:02:01 +0200

    Lots of improvements for merger.py script

--- merger.py
+++ merger.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
-#
-# Andrii Semkiv
-# 31.03.2016
-#
 
 import sys
+import os
+import argparse
 from array import array
 
-import ROOT
 from ROOT import TFile
 from ROOT import TTree
 
-def merge(files_and_numbers):
-    output_file = TFile('merged.root', 'recreate')
+def merge(files_and_numbers, output_file_name, verbose):
+    output_file = TFile(os.path.abspath(output_file_name), 'recreate')
     output_tree = TTree('Events', 'Events')
 
-    pi1_tauplus_px, pi1_tauplus_py, pi1_tauplus_pz = array('d', [0]), array('d', [0]), array('d', [0])
-    pi2_tauplus_px, pi2_tauplus_py, pi2_tauplus_pz = array('d', [0]), array('d', [0]), array('d', [0])
-    pi3_tauplus_px, pi3_tauplus_py, pi3_tauplus_pz = array('d', [0]), array('d', [0]), array('d', [0])
-    pi1_tauminus_px, pi1_tauminus_py, pi1_tauminus_pz = array('d', [0]), array('d', [0]), array('d', [0])
-    pi2_tauminus_px, pi2_tauminus_py, pi2_tauminus_pz = array('d', [0]), array('d', [0]), array('d', [0])
-    pi3_tauminus_px, pi3_tauminus_py, pi3_tauminus_pz = array('d', [0]), array('d', [0]), array('d', [0])
-    pi_k_px, pi_k_py, pi_k_pz = array('d', [0]), array('d', [0]), array('d', [0])
-    k_px, k_py, k_pz = array('d', [0]), array('d', [0]), array('d', [0])
-    pv_x, pv_y, pv_z = array('d', [0]), array('d', [0]), array('d', [0])
-    sv_x, sv_y, sv_z = array('d', [0]), array('d', [0]), array('d', [0])
-    tv_tauplus_x, tv_tauplus_y, tv_tauplus_z = array('d', [0]), array('d', [0]), array('d', [0])
-    tv_tauminus_x, tv_tauminus_y, tv_tauminus_z = array('d', [0]), array('d', [0]), array('d', [0])
+    # booking vars that the data from input files to be read in. Arrays are used due to some internal ROOT structure
+    pi1_tauplus_px, pi1_tauplus_py, pi1_tauplus_pz = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    pi2_tauplus_px, pi2_tauplus_py, pi2_tauplus_pz = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    pi3_tauplus_px, pi3_tauplus_py, pi3_tauplus_pz = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    pi1_tauminus_px, pi1_tauminus_py, pi1_tauminus_pz = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    pi2_tauminus_px, pi2_tauminus_py, pi2_tauminus_pz = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    pi3_tauminus_px, pi3_tauminus_py, pi3_tauminus_pz = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    pi_k_px, pi_k_py, pi_k_pz = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    k_px, k_py, k_pz = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    pv_x, pv_y, pv_z = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    sv_x, sv_y, sv_z = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    tv_tauplus_x, tv_tauplus_y, tv_tauplus_z = array('d', [0.]), array('d', [0.]), array('d', [0.])
+    tv_tauminus_x, tv_tauminus_y, tv_tauminus_z = array('d', [0.]), array('d', [0.]), array('d', [0.])
 
+    # creating branches in the output ROOT file and binding above variables to them
     output_tree.Branch('pi1_tauplus_px', pi1_tauplus_px, 'pi1_tauplus_px/D')
     output_tree.Branch('pi1_tauplus_py', pi1_tauplus_py, 'pi1_tauplus_py/D')
     output_tree.Branch('pi1_tauplus_pz', pi1_tauplus_pz, 'pi1_tauplus_pz/D')
@@ -76,16 +75,20 @@ def merge(files_and_numbers):
     output_tree.Branch('tv_tauminus_y', tv_tauminus_y, 'tv_tauminus_y/D')
     output_tree.Branch('tv_tauminus_z', tv_tauminus_z, 'tv_tauminus_z/D')
 
+    # iterating through files
     for f, n in files_and_numbers.iteritems():
-        print 'Processing file', f
-        input_file = TFile(f, 'read')
+        filepath = os.path.abspath(f)
+        print('Processing file {}'.format(filepath))
+        input_file = TFile(filepath, 'read')
         input_tree = input_file.Get('Events')
+
+        # iterating through events
         for counter, event in enumerate(input_tree):
             if counter < n:
-                if (counter + 1) % 100 == 0:
-                    print 'Processing event', counter + 1
+                if verbose and (counter + 1) % 100 == 0:
+                    print('Processing event {}'.format(counter + 1))
 
-                # Reading data necessary for reconstruction
+                # reading data and storing it in the output file branches
                 pi1_tauplus_px[0] = event.pi1_tauplus_px
                 pi1_tauplus_py[0] = event.pi1_tauplus_py
                 pi1_tauplus_pz[0] = event.pi1_tauplus_pz
@@ -140,17 +143,18 @@ def merge(files_and_numbers):
     output_file.Close()
 
 def main(argv):
-    argsdict = {}
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input-file', nargs = 2, action = 'append', required = True, help = 'input file name followed by number of events for that file')
+    parser.add_argument('-o', '--output-file', type = str, default = 'merged.root', help = 'output file name')
+    parser.add_argument('-v', '--verbose', action = 'store_true', default = False, help = 'run with increased verbosity')
 
-    try:
-        for f, n in zip(argv[1::2], argv[2::2]):
-            argsdict[f] = int(n)
-    except Exception as e:
-        print 'Something went wrong...'
-        print e
-        print 'Usage: merger.py FILENAME_1 NUMBER_OF_EVENTS_1 FILENAME_2 NUMBER_OF_EVENTS_2 ...'
+    args = parser.parse_args()
+    filesdict = {}
 
-    merge(argsdict)
+    for f in args.input_file:
+        filesdict.update({f[0] : f[1]})
+
+    merge(filesdict, args.output_file, args.verbose)
 
 if __name__ == '__main__':
     main(sys.argv)
